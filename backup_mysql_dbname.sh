a76a927a4da9000e64229002019c03bf9523aedd
vvtam <aiwife@gmail.com>
2020-03-27 10:12:51 +0800

    init
    init

---
+++ backup_mysql_dbname.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#------------------------------------------
+# description:  MySQL backup shell script
+# author:       tam
+# update： 2020/03/27
+#------------------------------------------
+
+USER="user"
+PASSWORD="passwd"
+DATABASE="database"
+DBHOST="1.2.3.4"
+MAIL="xx@xx.com"
+BACKUP_DIR=/data/backup/    #备份文件存储路径
+LOGFILE=/data/backup/mysql.log    #日志文件路径
+ 
+DATE=`date +%Y%m%d-%H%M`    #用日期格式作为文件名
+DUMPFILE=$DATABASE-$DATE.sql
+ARCHIVE=$DATABASE-$DATE.sql.tar.gz
+OPTIONS="-h$DBHOST -u$USER -p$PASSWORD $DATABASE"
+# OPTIONS="-h$DBHOST -u$USER -p$PASSWORD --set-gtid-purged=OFF $DATABASE"
+ 
+#备份目录是否存在，否则创建该目录
+if [ ! -d $BACKUP_DIR ]
+then
+	mkdir -p "$BACKUP_DIR"
+fi
+
+#写日志
+echo "    " >> $LOGFILE
+echo "--------------------" >> $LOGFILE
+echo "BACKUP DATE:" $(date +"%y-%m-%d %H:%M:%S") >> $LOGFILE
+echo "-------------------" >> $LOGFILE
+
+#切换至备份目录
+cd $BACKUP_DIR
+/usr/local/mysql/bin/mysqldump $OPTIONS > $DUMPFILE
+#判断数据库备份是否成功
+if [[ $? == 0 ]]
+then
+    tar czvf $ARCHIVE $DUMPFILE >> $LOGFILE 2>&1
+    echo "[$ARCHIVE] Backup Successful!" >> $LOGFILE
+    rm -f $DUMPFILE    #删除原始备份文件,只需保留备份压缩包
+    #把压缩包文件备份到其他机器上。
+    scp $ARCHIVE root@172.17.3.35:/home/mysql_backup/ >> $LOGFILE  2>&1
+else
+    echo "Database Backup Fail!" >> $LOGFILE
+    #备份失败后向管理者发送邮件提醒
+    #mail -s "database:$DATABASE Backup Fail!" $MAIL
+fi
+echo "Backup Process Done"
+#删除5天以上的备份文件
+find $BACKUP_DIR  -type f -mtime +5 -name "*.tar.gz" -exec rm -f {} \;
---
+++ backup_web_sitedir.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#-----------------------------------------
+#description: backup web site         
+#author:      tam
+#date:        2016/05/26
+#update：     2020/03/27
+#-----------------------------------------
+
+APP_DIR=/data/wwwroot
+BACKUP_APP=cms
+BACKUP_DATE=`date +%Y%m%d-%H%M`
+KEEP_DAYS=5
+
+BACKUP_DIR=/data/backup/
+ARCHIVE_FILE=$BACKUP_APP-$BACKUP_DATE.tar.gz
+LOGFILE=/data/backup/cms.log
+
+ADMIN_MAIL=
+
+#back to remote server
+#SERVER_NAME=
+SERVER_IP=192.168.1.155
+SERVER_PORT=22
+SERVER_USER=root
+SERVER_DIR=/data/backup
+##########################################
+
+# check the backup
+if [ ! -d $BACKUP_DIR ]
+then
+    mkdir -p "$BACKUP_DIR"
+fi
+
+#create log
+echo "--------------------" >> $LOGFILE
+echo "BACKUP DATE:" $BACKUP_DATE >> $LOGFILE
+
+#cd backup dir
+cd $APP_DIR
+tar czvf $BACKUP_DIR/$ARCHIVE_FILE $BACKUP_APP >> $LOGFILE 2>&1 
+echo "$ARCHIVE_FILE backup success" >> $LOGFILE
+cd $BACKUP_DIR
+scp -P $SERVER_PORT $ARCHIVE_FILE $SERVER_USER@$SERVER_IP:$SERVER_DIR >> $LOGFILE 2>&1
+#scp -P $SERVER_PORT $ARCHIVE_FILE $SERVER_USER@$SERVER_NAME:$SERVER_DIR >> $LOGFILE 2>&1
+
+echo "backup process done" >> $LOGFILE
+
+#delete the file over xx days
+find $BACKUP_DIR -type f -mtime +$KEEP_DAYS -name "*.tar.gz" -exec rm -f {} \;
